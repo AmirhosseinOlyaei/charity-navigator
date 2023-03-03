0c71ec9dcbfa3b0d79137158c7476ca1b8dd88b5
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-03-02 22:24:34 -0500

    drop down srcipt in react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Navbar from "./Navbar";
+import InputDD from "./InputDD";
 
 function App() {
     return (
         <div>
             <Navbar appName='Charity Navigator'/>
+            <InputDD appName='Charity Navigator'/>
+
         </div>
     );
 }
